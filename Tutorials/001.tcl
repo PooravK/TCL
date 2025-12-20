4f2580ff0475be148aea99f24cd04bc63dc2e1aa
PooravK <boomster.work@gmail.com>
2025-12-20 13:31:18 +0530

    Completed tutorial 1 of tcl

--- Tutorials/001.tcl
+++ Tutorials/001.tcl
@@ -1,1 +1,25 @@
+# tclsh -> Enter command line
+# exit
+
+#puts -> printing
+
 puts "Hello World!";
+
+# Semicolon not required when one single line of statement such as:
+
+puts "Hello World!"
+puts "Semicolon not required"
+
+# Semicolon is required when writing two commands in a single line such as:
+
+puts "Hello World!"; puts "Not Hello"
+
+# set is used for variable declaration
+
+# Example:
+
+set str "This is a string!";
+puts "$str";
+
+set var_value 1.5;
+puts "The value is $var_value";
